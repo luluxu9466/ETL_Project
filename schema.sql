ee26401688b6b297a75672450bd2e377d981b9c1
Nick L. Grove <90ngrove@gmail.com>
2019-12-12 20:40:49 -0500

    .

--- schema.sql
+++ schema.sql
@@ -4,9 +4,10 @@ DROP TABLE IF EXISTS insufficient_sleep;
 DROP TABLE IF EXISTS air_pollution;
 DROP TABLE IF EXISTS happiness_score;
 
+
 -- Create tables for raw data to be loaded into
 CREATE TABLE mental_illness (
-id INT PRIMARY KEY,
+--id INT PRIMARY KEY,
 mental_state TEXT,
 mental_measure_name TEXT,
 mental_rank integer,
@@ -16,7 +17,7 @@ mental_source_year TEXT
 );
 
 CREATE TABLE insufficient_sleep (
-id INT PRIMARY KEY,
+--id INT PRIMARY KEY,
 sleep_state TEXT,
 sleep_measure_name TEXT,
 sleep_rank INT,
@@ -26,7 +27,7 @@ sleep_source_year TEXT
 );
 
 CREATE TABLE air_pollution (
-id INT PRIMARY KEY,
+--id INT PRIMARY KEY,
 pollution_state TEXT,
 pollution_measure_name TEXT,
 pollution_rank INT,
@@ -36,12 +37,11 @@ pollution_source_year TEXT
 );
 
 CREATE TABLE happiness_score (
-id INT PRIMARY KEY,
+--id INT PRIMARY KEY,
 overall_rank INT,
 emotphys_rank INT,
 state TEXT,
 population INT
 );
-
 
 
