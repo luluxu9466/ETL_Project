d1adc4edbecbfecb074eb9aee1c3ad2d4a212b65
Fangyi Xu <fxu@caltech.edu>
2019-12-12 21:54:23 -0500

    final code

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 DROP TABLE IF EXISTS mental_illness;
 DROP TABLE IF EXISTS insufficient_sleep;
@@ -7,7 +6,7 @@ DROP TABLE IF EXISTS happiness_score;
 
 -- Create tables for raw data to be loaded into
 CREATE TABLE mental_illness (
-id INT PRIMARY KEY,
+-- id INT PRIMARY KEY,
 mental_state TEXT,
 mental_measure_name TEXT,
 mental_rank integer,
@@ -17,7 +16,7 @@ mental_source_year TEXT
 );
 
 CREATE TABLE insufficient_sleep (
-id INT PRIMARY KEY,
+-- id INT PRIMARY KEY,
 sleep_state TEXT,
 sleep_measure_name TEXT,
 sleep_rank INT,
@@ -27,7 +26,7 @@ sleep_source_year TEXT
 );
 
 CREATE TABLE air_pollution (
-id INT PRIMARY KEY,
+-- id INT PRIMARY KEY,
 pollution_state TEXT,
 pollution_measure_name TEXT,
 pollution_rank INT,
@@ -37,7 +36,7 @@ pollution_source_year TEXT
 );
 
 CREATE TABLE happiness_score (
-id INT PRIMARY KEY,
+-- id INT PRIMARY KEY,
 overall_rank INT,
 emotphys_rank INT,
 state TEXT,
@@ -46,52 +45,3 @@ population INT
 
 
 
-=======
-
-DROP TABLE IF EXISTS mental_illness;
-DROP TABLE IF EXISTS insufficient_sleep;
-DROP TABLE IF EXISTS air_pollution;
-DROP TABLE IF EXISTS happiness_score;
-
-
--- Create tables for raw data to be loaded into
-CREATE TABLE mental_illness (
---id INT PRIMARY KEY,
-mental_state TEXT,
-mental_measure_name TEXT,
-mental_rank integer,
-mental_Score FLOAT,
-mental_Source TEXT,
-mental_source_year TEXT
-);
-
-CREATE TABLE insufficient_sleep (
---id INT PRIMARY KEY,
-sleep_state TEXT,
-sleep_measure_name TEXT,
-sleep_rank INT,
-sleep_Score FLOAT,
-sleep_Source TEXT,
-sleep_source_year TEXT
-);
-
-CREATE TABLE air_pollution (
---id INT PRIMARY KEY,
-pollution_state TEXT,
-pollution_measure_name TEXT,
-pollution_rank INT,
-pollution_score FLOAT,
-pollution_source TEXT,
-pollution_source_year TEXT
-);
-
-CREATE TABLE happiness_score (
---id INT PRIMARY KEY,
-overall_rank INT,
-emotphys_rank INT,
-state TEXT,
-population INT
-);
-
-
->>>>>>> ee26401688b6b297a75672450bd2e377d981b9c1
